1e100d9506410eba3c59c70c04bc4ec1492b2cb1
Sogo <Sogo@Jihoons-iMac-3.local>
2014-11-30 15:02:09 +0900

    added color detection

--- Color_Extraction.py
+++ Color_Extraction.py
@@ -1,4 +1,36 @@
+"""
+    Sogang University Datamining Laboratory
+    title: Personal color Extratcion
+    author: sogo
+"""
 # Personal Color Extraction
-__author__ = 'Sogo'
+import cv2
+import numpy as np
+from collections import Counter
+from find_suitable_color import *
 
-print 'hello'
+# read image
+targetImage = cv2.imread('./Test_Images/YH334_face.jpg')
+
+skinCandidates = []
+# found color range and count it from skin color range
+for x in range(len(targetImage)):
+    for y in range(len(targetImage[x])):
+        skinColor = color_detector(targetImage[x][y])
+        if skinColor is not -1:
+            skinCandidates.append(skinColor)
+# found a mean color
+a = Counter(skinCandidates).most_common(4)
+# print out BGR
+color1 = (skinMap[a[0][0]][2], skinMap[a[0][0]][1], skinMap[a[0][0]][0])
+color2 = (skinMap[a[1][0]][2], skinMap[a[1][0]][1], skinMap[a[1][0]][0])
+color3 = (skinMap[a[2][0]][2], skinMap[a[2][0]][1], skinMap[a[2][0]][0])
+color4 = (skinMap[a[3][0]][2], skinMap[a[3][0]][1], skinMap[a[3][0]][0])
+colorWinodw = np.zeros((200, 200, 3), np.uint8)
+cv2.rectangle(colorWinodw, (0, 0), (99, 99), color1, -1)
+cv2.rectangle(colorWinodw, (100, 0), (199, 99), color2, -1)
+cv2.rectangle(colorWinodw, (0, 100), (99, 199), color3, -1)
+cv2.rectangle(colorWinodw, (100, 100), (199, 199), color4, -1)
+cv2.imshow("Image", targetImage)
+cv2.imshow("Color", colorWinodw)
+cv2.waitKey(0)
